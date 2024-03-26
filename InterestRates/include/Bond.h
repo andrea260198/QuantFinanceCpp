07dc18766b4c3a16a4610cb22dd305b9b1533454
Andrea Cassotti <cassottiandrea@hotmail.com>
2024-03-26 20:09:20 +0100

    Tabs changed with spaces

--- InterestRates/include/Bond.h
+++ InterestRates/include/Bond.h
@@ -12,8 +12,8 @@ class Bond
 public:
     Bond(double mat)
         : maturity(mat)
-	{
-	}
+    {
+    }
 
     double calcBondPrice();
     double calcYield();
@@ -23,17 +23,17 @@ private:
     double calcExponentialOfIntegral(int seed);
 
     CoxIngersollRossModel model;
-	double bondPrice;
-	double r0 = 0.1;
-	double r;
-	double dt = 0.001;
-	double a = 0.20;
-	double b = 0.05;
-	double sigma = 0.03;
-	double dr;
-	double integral_r_dt;
+    double bondPrice;
+    double r0 = 0.1;
+    double r;
+    double dt = 0.001;
+    double a = 0.20;
+    double b = 0.05;
+    double sigma = 0.03;
+    double dr;
+    double integral_r_dt;
     int M = 20;
-	int seed = 0;
+    int seed = 0;
     double maturity = 10;
 };
 
