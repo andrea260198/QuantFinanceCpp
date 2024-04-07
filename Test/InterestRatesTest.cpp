df89a6747bf7541f1b32e7d7e5a50e306836fcc3
Andrea Cassotti <cassottiandrea@hotmail.com>
2024-04-07 22:54:43 +0200

    Unit test fixed

--- Test/InterestRatesTest.cpp
+++ Test/InterestRatesTest.cpp
@@ -8,8 +8,8 @@ TEST(BondPricing, YieldCalculation)
     Bond bond2(10);
 
     //EXPECT_EQ(bond.calcYield(), 0.074);
-    ASSERT_NEAR(bond1.calcYield(), 0.0834, 0.0001);
-    ASSERT_NEAR(bond2.calcYield(), 0.0740, 0.0001);
+    ASSERT_NEAR(bond1.calcYield(), 0.0834, 0.001);
+    ASSERT_NEAR(bond2.calcYield(), 0.0740, 0.001);
 }
 
 /*
